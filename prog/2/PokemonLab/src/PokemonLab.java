049e2c556b24933fad4318847a514ca4ddb9ec86
Dmitry <dmitry.kuper0@gmail.com>
2019-10-10 19:44:11 +0300

    _

--- prog/2/PokemonLab/src/PokemonLab.java
+++ prog/2/PokemonLab/src/PokemonLab.java
@@ -8,12 +8,14 @@ public class PokemonLab {
         Pokemon ally2 = new Diggersby("Здравый смысл", 53);
         Pokemon ally3 = new Marill("Кожанка", 87);
         Pokemon foe1 = new Bunnelby("Поляков", 47);
-        Pokemon foe2 = new Azurill("Неклассика", 87);
+        Pokemon foe2 = new Azurill("Методичка", 87);
+        Pokemon foe3 = new Azumarill("ПСЖ", 21);
         b.addAlly(ally1);
         b.addAlly(ally2);
         b.addAlly(ally3);
         b.addFoe(foe1);
         b.addFoe(foe2);
+        b.addFoe(foe3);
         b.go();
     }
 }
---
+++ prog/2/PokemonLab/src/pokemons/Azumarill.java
@@ -0,0 +1,22 @@
+package pokemons;
+
+import moves.BellyDrum;
+import moves.Swagger;
+import moves.WaterGun;
+import ru.ifmo.se.pokemon.Pokemon;
+import ru.ifmo.se.pokemon.Type;
+
+public class Azumarill extends Pokemon {
+    public Azumarill(String name, int level) {
+        super(name, level);
+
+        setStats(100,50,80,60,80,50);
+
+        setType(Type.WATER, Type.FAIRY);
+
+        addMove(new WaterGun());
+        addMove(new Swagger());
+        addMove(new BellyDrum());
+        addMove(new WaterGun());
+    }
+}
