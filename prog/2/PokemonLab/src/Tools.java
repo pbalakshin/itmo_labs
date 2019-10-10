31c3ac2388106becc20f037f48fa0a88127e7038
kupp <dmitry.kuper0@gmail.com>
2019-10-10 19:54:28 +0300

    _

--- prog/2/PokemonLab/src/Tools.java
+++
@@ -1,9 +0,0 @@
-import ru.ifmo.se.pokemon.Pokemon;
-import ru.ifmo.se.pokemon.Stat;
-
-/*public class Tools { FIXME
-    public static void setOneStat(Pokemon pokemon, Stat statForChange, double NewStatValue) {
-        currentAccuracy = pokemon.getStat(Stat.ACCURACY);
-
-    }
-}*/
